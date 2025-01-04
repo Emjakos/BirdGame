dd1b8ca6bfd1edfbd79aed0f1fbfbaa5e38e322e
sleepwalkers1 <patrik.astrom1@yahoo.com>
2025-01-05 01:27:53 +0200

    Added treespawner update

--- Assets/treespawner.cs
+++ Assets/treespawner.cs
@@ -2,7 +2,7 @@ using UnityEngine;
 
 public class treespawner : MonoBehaviour
 {
-    int c;
+    int c, YZposition;
     public GameObject tree1, tree2, tree3, tree4;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -10,16 +10,20 @@ public class treespawner : MonoBehaviour
         for(c = 0; c <= 40; c++)
         {
             //first tree randomizer
-            Vector3 randompspawnpoint1 = new Vector3(Random.Range(-10, 40), Random.Range(-10, 40), 0);
+            YZposition = Random.Range(-10, 40);
+            Vector3 randompspawnpoint1 = new Vector3(Random.Range(-10, 40), YZposition, YZposition);
             Instantiate(tree1, randompspawnpoint1, Quaternion.identity);
             //second tree randomizer
-            Vector3 randompspawnpoint2 = new Vector3(Random.Range(-10, 40), Random.Range(-10, 40), 0);
+            YZposition = Random.Range(-10, 40);
+            Vector3 randompspawnpoint2 = new Vector3(Random.Range(-10, 40), YZposition, YZposition);
             Instantiate(tree2, randompspawnpoint2, Quaternion.identity);
             //third tree randomizer
-            Vector3 randompspawnpoint3 = new Vector3(Random.Range(-10, 40), Random.Range(-10, 40), 0);
+            YZposition = Random.Range(-10, 40);
+            Vector3 randompspawnpoint3 = new Vector3(Random.Range(-10, 40), YZposition, YZposition);
             Instantiate(tree3, randompspawnpoint3, Quaternion.identity);
             //fourth tree randomizer
-            Vector3 randompspawnpoint4 = new Vector3(Random.Range(-10, 40), Random.Range(-10, 40), 0);
+            YZposition = Random.Range(-10, 40);
+            Vector3 randompspawnpoint4 = new Vector3(Random.Range(-10, 40), YZposition, YZposition);
             Instantiate(tree4, randompspawnpoint4, Quaternion.identity);
         }
     }
