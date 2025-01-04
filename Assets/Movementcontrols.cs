9caea063abfc5b3f621e4a35a75344943dd4faf9
sleepwalkers1 <patrik.astrom1@yahoo.com>
2025-01-04 21:05:48 +0200

    Movement

---
+++ Assets/Movementcontrols.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+
+public class Movementcontrols : MonoBehaviour
+{
+    public float constantspeed = 2.0f;
+    public float speed = 10.0f;
+    public float rotationSpeed = 100.0f;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+    //yes
+    // Update is called once per frame
+    void Update()
+    {
+        // Get the horizontal and vertical axis.
+        // By default they are mapped to the arrow keys.
+        // The value is in the range -1 to 1
+        float translation = Input.GetAxis("Vertical") * speed;
+        float rotation = Input.GetAxis("Horizontal") * rotationSpeed;
+
+        // Make it move 10 meters per second instead of 10 meters per frame...
+        translation *= Time.deltaTime;
+        rotation *= Time.deltaTime;
+        //constant movementspeed without pressing buttons
+        transform.Translate(0, constantspeed * Time.deltaTime, 0);
+        // Move translation along the object's z-axis
+        transform.Translate(0, translation, 0);
+
+        // Rotate around our y-axis
+        transform.Rotate(0, 0, rotation);
+    }
+}
